6ba195fda08f99c2148d4b8370c313f29dbf11dc
DiegoFelipa <1342318@senati.pe>
2023-05-23 11:26:28 -0500

    sidebar-hide

--- views/login.php
+++ views/login.php
@@ -63,7 +63,7 @@ session_start();
 
 	<!-- JQUERY -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<script src="../assets/js/popper.js"></script>
+	
 	<script src="../assets/js/bootstrap.min.js"></script>
 	<script src="../assets/js/main.js"></script>
 	<!-- FONTWASOME -->
