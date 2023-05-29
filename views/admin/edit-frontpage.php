6f24a65ea5be718e6f4e2cd1bd089bdcbf06cbe8
AriasDeve <alexander171194@gmail.com>
2023-05-29 09:01:45 -0500

    EditPortada

--- views/admin/edit-frontpage.php
+++ views/admin/edit-frontpage.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <title>Administrador</title>
+    <title>Editar</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="../../assets/css/popups.css" />
@@ -30,10 +30,10 @@ session_start();
                             <i class="fas fa-bars"></i>
                         </button>
                         <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                            <font face="impact"><h2 >Editar PDF</h2></font>
+                            <font face="impact"><h2 >Editar Portada</h2></font>
                         </form>
                         <ul class="navbar-nav flex-nowrap ml-auto">
-                            <!-- <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="true" data-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+                            <!-- <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded=true" data-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
                                 <div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" aria-labelledby="searchDropdown">
                                     <form class="form-inline mr-auto navbar-search w-100">
                                         <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
@@ -86,17 +86,17 @@ session_start();
                                     </div>
                                 </div>
                                 <div class="form-group text-center">
-                                    <button type="button" id="cancelar-pdf" class="btn btn-sm btn-secondary" class="close">
+                                    <button type="button" id="cancelar-modal-portada" class="btn btn-sm btn-secondary" class="close"data-dismiss="modal">
                                         Cancelar
                                     </button>
-                                    <button type="submit" id="guardar-pdf" class="btn btn-sm btn-primary">
+                                    <button type="submit" id="guardar-portada-editar" class="btn btn-sm btn-primary">
                                         Modificar
                                     </button>
                                 </div>
                             </div>
                             <div class="col-md-8">
                                 <div class="pdf mt-5 ml-5">
-                                    <iframe src="../../views/PDF/0e4aaa4dfe0bdaf1a67476f85e99329219502f1c.pdf" width="80%" height="500"></iframe>
+                                    <img src="../../views/frontpage/80800719ad75ad6290190dd1bc1fa25ad7a0e04b.jpg" width="50%" height="450">
                                 </div>
                             </div>
                         </div>
