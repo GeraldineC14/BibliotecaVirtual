b28452c480a20a4536b9156cbb2833b10e74740c
AriasDeve <alexander171194@gmail.com>
2023-07-07 15:21:03 -0500

    formato Reportes

--- reports/report-subcategoria/datos.php
+++ reports/report-subcategoria/datos.php
@@ -1,37 +1,31 @@
 <page orientation="portrait">
-    <bookmark title="Document" level="0"></bookmark>
-    <a name="document_reprise"></a>
     <table cellspacing="0" style="width: 100%;">
         <tr>
-            <td style="width: 10%;">
-                <img style="width: 100%" src="../../assets/img/Insignia.png" alt="Logo Html2Pdf">
-            </td>
-            <td style="width: 80%; font-weight: bold;">
-                <span style="font-size: 10pt"><br></span>
+            <td style="width: 100%; font-weight: bold;">
                 <div class="center" style="font-size: 20pt;">
-                    REPORTE DE SUB CATEGORÍA
+                    REPORTE DE SUBCATEGORÍA
                 </div>
-            </td>
-            <td style="width: 10%;">
             </td>
         </tr>
     </table>
     <table class="mt-3" cellspacing="0" style="width: 100%; justify-content: center;">
         <tr>
-            <td style="width: 55%; ">
-                <table cellspacing="0" style="width: 95%; border: solid 2px #000000; ">
+            <td style="width: 100%; ">
+                <table cellspacing="0" style="width: 98%; border: solid 2px #000000; ">
                     <tr>
-                        <td style="width: 100%; font-size: 12pt; padding-left:10px; padding-top: 10px;">
+                        <td class="pl-3 pt-1" style="width: 10%;">
+                            <img style="width: 8%" src="../../assets/img/Insignia.png" alt="Logo Html2Pdf">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 100%; font-size: 12pt;  padding-top: 10px;">
                             <div class="center">
                                 <span style="font-size: 16pt; font-weight: bold;">JEC Horacio Zeballos Gámez<br></span>
                                 <span style="margin-top: 3px; font-size: 12pt;">BIBLIOTECA VIRTUAL</span>
                             </div>
-                            <br>
-                            <b>Encargado de Biblioteca :</b> Richard<br>
-                            <b>Tel :</b> 999 999 999<br>
-                            <b>Email :</b> admin@gmail.com<br>
                             <div class="end" style="margin-right: 15px; margin-bottom: 5px;">
-                                <b> Fecha : </b><?php echo date('d/m/Y'); ?><br>
+                                <b> Fecha : </b>
+                                <?php echo date('d/m/Y'); ?><br>
                             </div>
                         </td>
                     </tr>
@@ -39,101 +33,110 @@
             </td>
         </tr>
         <tr>
-            <td style="width:55%;">
-                
-                <br>
+            <td style="width:100%;">
                 <!-- Header -->
                 <table cellspacing="0" style="width: 100%; text-align: left; font-size: 8pt">
                     <tr>
                         <td style="width: 100%">
                             <div>
                                 <?php
-                                    function crearTabla($categoria = ""){
-                                        $nuevaTabla = "
+                                function crearTabla($categoria = "")
+                                {
+                                    $nuevaTabla = "
                                         <h4 class='mb-2 mt-5'>{$categoria}</h4>
-                                        <table class='table table-border mb-3'>
+                                        <table class='table table-border mb-3 custom-table'>
                                             <colgroup>
                                                   <col width='5%'>
-                                                  <col width='25%'>
-                                                  <col width='20%'>
-                                                  <col width='20%'>
-                                                  <col width='15%'>
-                                                  <col width='15%'>
+                                                  <col class='col-categoria'>
+                                                  <col class='col-fecha'>
                                               </colgroup>
                                               <thead>
-                                                  <tr class='bg-primary'>
+                                                  <tr class='bg-danger'>
                                                       <th>#</th>
                                                       <th>Sub Categoría</th>
-                                                      <th>Fecha de Registro</th>
+                                                      <th class='center'>Fecha de Registro</th>
                                                   </tr>
                                               </thead>
                                             <tbody>
                                         ";
                                     echo  $nuevaTabla;
-                                    }
-                            
-                                    function cerrarTabla(){
-                                        $cerrarTabla = "
+                                }
+
+                                function cerrarTabla()
+                                {
+                                    $cerrarTabla = "
                                         </tbody>
                                         </table>
                                         ";
                                     echo $cerrarTabla;
-                                    }
-                            
-                                    function agregarFila($arreglo = []) {
-                                      echo "
+                                }
+
+                                function agregarFila($arreglo = [])
+                                {
+                                    echo "
                                           <tr>
-                                              <td>{$arreglo['idsubcategorie']}</td>
+                                            <b><td>{$arreglo['idsubcategorie']}</td></b>
                                               <td>{$arreglo['subcategoryname']}</td>
-                                              <td>{$arreglo['registrationdate']}</td>
+                                              <td class='center'>{$arreglo['registrationdate']}</td>
                                           </tr>
                                       ";
-                                  }
-                                  
-                                    function reporteSubcategoria($registrosCategoria) {
-                                        echo "<h3 class='mt-3'>Número de Sub Categoría por Categorías:</h3>";
-                                        echo "<ul>";
-                                        
-                                        foreach ($registrosCategoria as $id => $numRegistros) {
-                                            echo "<li>{$id}: {$numRegistros} registros</li>";
-                                        }
-                                        echo "</ul>";
+                                }
+
+                                function reporteSubcategoria($registrosCategoria)
+                                {
+                                    echo "<h3 class='mt-4 mb-2'>RESUMEN DE INFORME</h3>";
+                                    echo "<table class='table'>";
+                                    echo "<thead><tr><th>Categoría</th><th>Número de Registros</th></tr></thead>";
+                                    echo "<tbody>";
+
+                                    foreach ($registrosCategoria as $id => $numRegistros) {
+                                        echo "<tr><td>{$id}</td><td class='center'>{$numRegistros}</td></tr>";
                                     }
-                            
-                                    if (count($datos) > 0){
-                                        $categoria = $datos[0]["categoryname"];
-                                        $registrosCategoria= array($categoria => 0);
-                                        crearTabla($categoria);
-                                        foreach($datos as $registro){
-                                            if($categoria == $registro["categoryname"]){
-                                                agregarFila($registro);
-                                                $registrosCategoria[$categoria]++;
-                                            }else{
-                                                $categoria = $registro["categoryname"];
-                                                cerrarTabla();
-                                                crearTabla($categoria);
-                                                agregarFila($registro);
-                                                $registrosCategoria[$categoria] = 1;
-                                            }
+
+                                    echo "</tbody>";
+                                    echo "</table>";
+                                }
+
+
+                                if (count($datos) > 0) {
+                                    $categoria = $datos[0]["categoryname"];
+                                    $registrosCategoria = array($categoria => 0);
+                                    crearTabla($categoria);
+                                    foreach ($datos as $registro) {
+                                        if ($categoria == $registro["categoryname"]) {
+                                            agregarFila($registro);
+                                            $registrosCategoria[$categoria]++;
+                                        } else {
+                                            $categoria = $registro["categoryname"];
+                                            cerrarTabla();
+                                            crearTabla($categoria);
+                                            agregarFila($registro);
+                                            $registrosCategoria[$categoria] = 1;
                                         }
-                                        cerrarTabla();
-                                        // Llamamos las funciones
-                                        reporteSubcategoria($registrosCategoria);
-                                    }else{
-                                        echo "<h3 class='mt-3'>No encontramos registros</h3>";
                                     }
+                                    cerrarTabla();
+                                    // Llamamos las funciones
+                                    reporteSubcategoria($registrosCategoria);
+                                } else {
+                                    echo "<h3 class='mt-3'>No encontramos registros</h3>";
+                                }
                                 ?>
                             </div>
                         </td>
                     </tr>
                 </table>
-                <br>
-                <!-- Footer  -->
-               
             </td>
-            <td style="width: 4%"></td>
-              
         </tr>
     </table>
+    <page_footer>
+        <table cellspacing="0" style="width: 100%;">
+            <tr>
+                <td style="font-size: 9pt;">
+                    <div class="end mt-1">
+                        [[page_cu]]/[[page_nb]]
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </page_footer>
 </page>
-
--- reports/report-user/datos.php
+++ reports/report-user/datos.php
@@ -1,37 +1,31 @@
 <page orientation="portrait">
-    <bookmark title="Document" level="0"></bookmark>
-    <a name="document_reprise"></a>
     <table cellspacing="0" style="width: 100%;">
         <tr>
-            <td style="width: 10%;">
-                <img style="width: 100%" src="../../assets/img/Insignia.png" alt="Logo Html2Pdf">
-            </td>
-            <td style="width: 80%; font-weight: bold;">
-                <span style="font-size: 10pt"><br></span>
+            <td style="width: 100%; font-weight: bold;">
                 <div class="center" style="font-size: 20pt;">
                     REPORTE DE USUARIOS
                 </div>
-            </td>
-            <td style="width: 10%;">
             </td>
         </tr>
     </table>
     <table class="mt-3" cellspacing="0" style="width: 100%; justify-content: center;">
         <tr>
-            <td style="width: 55%; ">
-                <table cellspacing="0" style="width: 95%; border: solid 2px #000000; ">
+            <td style="width: 100%; ">
+                <table cellspacing="0" style="width: 98%; border: solid 2px #000000; ">
                     <tr>
-                        <td style="width: 100%; font-size: 12pt; padding-left:10px; padding-top: 10px;">
+                        <td class="pl-3 pt-1" style="width: 10%;">
+                            <img style="width: 8%" src="../../assets/img/Insignia.png" alt="Logo Html2Pdf">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 100%; font-size: 12pt;  padding-top: 10px;">
                             <div class="center">
                                 <span style="font-size: 16pt; font-weight: bold;">JEC Horacio Zeballos Gámez<br></span>
                                 <span style="margin-top: 3px; font-size: 12pt;">BIBLIOTECA VIRTUAL</span>
                             </div>
-                            <br>
-                            <b>Encargado de Biblioteca :</b> Richard<br>
-                            <b>Tel :</b> 999 999 999<br>
-                            <b>Email :</b> admin@gmail.com<br>
                             <div class="end" style="margin-right: 15px; margin-bottom: 5px;">
-                                <b> Fecha : </b><?php echo date('d/m/Y'); ?><br>
+                                <b> Fecha : </b>
+                                <?php echo date('d/m/Y'); ?><br>
                             </div>
                         </td>
                     </tr>
@@ -39,122 +33,134 @@
             </td>
         </tr>
         <tr>
-            <td style="width:55%;">
-                
-                <br>
+            <td style="width:100%;">
                 <!-- Header -->
                 <table cellspacing="0" style="width: 100%; text-align: left; font-size: 8pt">
                     <tr>
                         <td style="width: 100%">
                             <div>
                                 <?php
-                                    function crearTabla($accesslevel = ""){
-                                        if($accesslevel == 'A'){
-                                            $accesslevel = 'Administrador';
-                                          }else if( $accesslevel == 'E'){
-                                            $accesslevel = 'Estudiante';
-                                          }else{
-                                            $accesslevel = 'Docente';
-                                          }
-                                        $nuevaTabla = "
+                                function crearTabla($accesslevel = "")
+                                {
+                                    if ($accesslevel == 'A') {
+                                        $accesslevel = 'Administrador';
+                                    } else if ($accesslevel == 'E') {
+                                        $accesslevel = 'Estudiante';
+                                    } else {
+                                        $accesslevel = 'Docente';
+                                    }
+                                    $nuevaTabla = "
                                         <h4 class='mb-2 mt-5'>{$accesslevel}</h4>
-                                        <table class='table table-border mb-3'>
+                                        <table class='table table-border mb-3 custom-table'>
                                             <colgroup>
-                                                  <col width='5%'>
-                                                  <col width='25%'>
-                                                  <col width='20%'>
-                                                  <col width='20%'>
-                                                  <col width='15%'>
-                                                  <col width='15%'>
-                                              </colgroup>
-                                              <thead>
-                                                  <tr class='bg-primary'>
-                                                      <th>ID</th>
-                                                      <th>Nombres</th>
-                                                      <th>Apellidos</th>
-                                                      <th>Nombre Usuario</th>
-                                                      <th>Email</th>
-                                                      <th>Nivel Acceso</th>
-                                                  </tr>
-                                              </thead>
+                                                <col class='col-id'>
+                                                <col class='col-nombres'>
+                                                <col class='col-apellidos'>
+                                                <col class='col-nombre-usuario'>
+                                                <col class='col-email'>
+                                                <col class='col-nivel-acceso'>
+                                            </colgroup>
+                                            <thead>
+                                                <tr class='bg-danger center'>
+                                                    <th class='center'>ID</th>
+                                                    <th>Nombres</th>
+                                                    <th>Apellidos</th>
+                                                    <th>Usuario</th>
+                                                    <th>Email</th>
+                                                    <th>Nivel Acceso</th>
+                                                </tr>
+                                            </thead>
                                             <tbody>
                                         ";
-                                    echo  $nuevaTabla;
-                                    }
-                            
-                                    function cerrarTabla(){
-                                        $cerrarTabla = "
-                                        </tbody>
+                                    echo $nuevaTabla;
+                                }
+
+                                function cerrarTabla()
+                                {
+                                    $cerrarTabla = "
+                                            </tbody>
                                         </table>
                                         ";
                                     echo $cerrarTabla;
-                                    }
-                            
-                                    function agregarFila($arreglo = []) {
-                                      echo "
-                                          <tr>
-                                              <td>{$arreglo['idusers']}</td>
-                                              <td>{$arreglo['namess']}</td>
-                                              <td>{$arreglo['surnames']}</td>
-                                              <td>{$arreglo['username']}</td>
-                                              <td>{$arreglo['email']}</td>
-                                              <td>{$arreglo['accesslevel']}</td>
-                                          </tr>
-                                      ";
-                                  }
-                                  
-                                    function reportUser($registrosAccesslevel) {
-                                        echo "<h3 class='mt-3'>Número de registros por Usuario:</h3>";
-                                        echo "<ul>";
-                                        
-                                        foreach ($registrosAccesslevel as $id => $numRegistros) {
-                                            if($id == 'A'){
-                                                $id = 'Administrador';
-                                              }else if( $id == 'E'){
-                                                $id = 'Estudiante';
-                                              }else{
-                                                $id = 'Docente';
-                                              }
-                                            echo "<li>{$id}: {$numRegistros} registros</li>";
+                                }
+
+                                function agregarFila($arreglo = [])
+                                {
+                                    echo "
+                                            <tr>
+                                                <b><td class='center'>{$arreglo['idusers']}</td></b>
+                                                <td>{$arreglo['namess']}</td>
+                                                <td>{$arreglo['surnames']}</td>
+                                                <td >{$arreglo['username']}</td>
+                                                <td>{$arreglo['email']}</td>
+                                                <td class='center'>{$arreglo['accesslevel']}</td>
+                                            </tr>
+                                        ";
+                                }
+
+                                function reportUser($registrosAccesslevel)
+                                {
+                                    echo "<h3 class='mt-4 mb-2'>RESUMEN DE INFORME</h3>";
+                                    echo "<table class='table'>";
+                                    echo "<thead><tr><th>Usuario</th><th>Número de Registros</th></tr></thead>";
+                                    echo "<tbody>";
+
+                                    foreach ($registrosAccesslevel as $id => $numRegistros) {
+                                        if ($id == 'A') {
+                                            $id = 'Administrador';
+                                        } else if ($id == 'E') {
+                                            $id = 'Estudiante';
+                                        } else {
+                                            $id = 'Docente';
                                         }
-                                        echo "</ul>";
+                                        echo "<tr><td>{$id}</td><td>{$numRegistros} registro(s)</td></tr>";
                                     }
-                            
-                                    if (count($datos) > 0){
-                                        
-                                        $rolActual = $datos[0]["accesslevel"];
-                                        $registrosAccesslevel = array($rolActual => 0);
-                                        crearTabla($rolActual);
-                                        foreach($datos as $registro){
-                                            if($rolActual == $registro["accesslevel"]){
-                                                agregarFila($registro);
-                                                $registrosAccesslevel[$rolActual]++;
-                                            }else{
-                                                $rolActual = $registro["accesslevel"];
-                                                cerrarTabla();
-                                                crearTabla($rolActual);
-                                                agregarFila($registro);
-                                                $registrosAccesslevel[$rolActual] = 1;
-                                            }
+
+                                    echo "</tbody>";
+                                    echo "</table>";
+                                }
+
+
+                                if (count($datos) > 0) {
+
+                                    $rolActual = $datos[0]["accesslevel"];
+                                    $registrosAccesslevel = array($rolActual => 0);
+                                    crearTabla($rolActual);
+                                    foreach ($datos as $registro) {
+                                        if ($rolActual == $registro["accesslevel"]) {
+                                            agregarFila($registro);
+                                            $registrosAccesslevel[$rolActual]++;
+                                        } else {
+                                            $rolActual = $registro["accesslevel"];
+                                            cerrarTabla();
+                                            crearTabla($rolActual);
+                                            agregarFila($registro);
+                                            $registrosAccesslevel[$rolActual] = 1;
                                         }
-                                        cerrarTabla();
-                                        // Llamamos las funciones
-                                        reportUser($registrosAccesslevel);
-                                    }else{
-                                        echo "<h3 class='mt-3'>No encontramos registros</h3>";
                                     }
+                                    cerrarTabla();
+                                    // Llamamos las funciones
+                                    reportUser($registrosAccesslevel);
+                                } else {
+                                    echo "<h3 class='mt-3'>No encontramos registros</h3>";
+                                }
                                 ?>
                             </div>
                         </td>
                     </tr>
                 </table>
-                <br>
-                <!-- Footer  -->
-               
             </td>
-            <td style="width: 4%"></td>
-              
         </tr>
     </table>
+    <page_footer>
+        <table cellspacing="0" style="width: 100%;">
+            <tr>
+                <td style="font-size: 9pt;">
+                    <div class="end mt-1">
+                        [[page_cu]]/[[page_nb]]
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </page_footer>
 </page>
-
